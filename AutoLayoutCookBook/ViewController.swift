2b400972409bc193601ef8859aacc9570aa50017
fsaint <fsaint@gmail.com>
2014-10-28 08:17:30 -0400

    Initial Commit

---
+++ AutoLayoutCookBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutCookBook
+//
+//  Created by Felipe Saint-Jean on 10/28/14.
+//  Copyright (c) 2014 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
