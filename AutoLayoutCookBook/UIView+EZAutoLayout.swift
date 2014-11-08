a51fa1ae99b21ae27ffce26bf304bf8bdb5f134c
fsaint <fsaint@gmail.com>
2014-11-08 12:14:14 -0500

    All autolayout operations working

--- AutoLayoutCookBook/UIView+EZAutoLayout.swift
+++ AutoLayoutCookBook/UIView+EZAutoLayout.swift
@@ -10,28 +10,25 @@ import Foundation
 import UIKit
 extension UIView {
     func ezClearConstraints(){
-        
-        
+        self.setTranslatesAutoresizingMaskIntoConstraints(false)
         
         for c in self.superview!.constraints(){
             
             if let const = c as? NSLayoutConstraint {
                 if (const.firstItem === self && (const.secondItem === self.superview || const.secondItem == nil) ){
                     self.superview!.removeConstraint(const)
-                }
-                
-                
-                if (const.secondItem === self && (const.firstItem === self.superview)){
+                }else if (const.secondItem === self && (const.firstItem === self.superview)){
                     self.superview!.removeConstraint(const)
                 }
                 
             }
             
         }
+
         
         self.removeConstraints(self.constraints())
         
-        self.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
     }
     
     //MARK: Utility methods to position a view in reltion to it's parent with no size change
@@ -65,60 +62,59 @@ extension UIView {
 
     func ezRight(margin: Int = 0){
        
-        self.ezClearConstraints()
         self._alignSuperView("V:[view(height)]", horizontal: "H:[view(width)]-margin-|", margin: margin, attribute: NSLayoutAttribute.CenterY)
         
     }
     
     func ezTop(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:|-margin-[view(height)]", horizontal: "H:[view(width)]", margin: margin, attribute: NSLayoutAttribute.CenterX)
         
     }
     
     func ezBottom(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:[view(height)]-margin-|", horizontal: "H:[view(width)]", margin: margin, attribute: NSLayoutAttribute.CenterX)
 
     }
     
     func ezLeft(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:[view(height)]", horizontal: "H:|-margin-[view(width)]", margin: margin, attribute: NSLayoutAttribute.CenterY)
         
     }
     
     func ezTopLeft(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:|-margin-[view(height)]", horizontal: "H:|-margin-[view(width)]", margin:margin)
         
     }
     
     func ezTopRight(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:|-margin-[view(height)]", horizontal: "H:[view(width)]-margin-|", margin:margin)
         
         
     }
     func ezBottomRight(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:[view(height)]-margin-|", horizontal: "H:[view(width)]-margin-|", margin:margin)
         
     }
     func ezBottomLeft(margin: Int = 0){
-        self.ezClearConstraints()
         self._alignSuperView("V:[view(height)]-margin-|", horizontal: "H:|-margin-[view(width)]", margin:margin)
         
     }
    
     
     
-    func ezCenter(){
+    func ezCenter(width:Int? = nil, height:Int? = nil){
         self.ezClearConstraints()
         
         var views: NSMutableDictionary = NSMutableDictionary()
         views["view"] = self
-        var metrics = ["width":self.frame.width,"height":self.frame.height]
+        var metrics = [
+            "width": (width != nil ? width! : Int(self.frame.width)),
+            "height": (height != nil ? height! : Int(self.frame.height))
+        ]
+        
+        
+        
+        
         
         
         var v_constrains  = NSLayoutConstraint.constraintsWithVisualFormat("V:[view(height)]", options: NSLayoutFormatOptions(0), metrics: metrics, views: views)
@@ -144,6 +140,7 @@ extension UIView {
     
     
     func ezFill(margin: Int = 0){
+        self.ezClearConstraints()
         var views: NSMutableDictionary = NSMutableDictionary()
         views["view"] = self
         
@@ -161,7 +158,7 @@ extension UIView {
     }
     
     func ezTopFill(margin: Int = 0){
-    
+        self.ezClearConstraints()
         var views: NSMutableDictionary = NSMutableDictionary()
         views["view"] = self
         
@@ -178,7 +175,7 @@ extension UIView {
     }
     
     func ezLeftFill(margin: Int = 0){
-        self._alignSuperView("V:|-margin-[view(height)]-margin-|", horizontal: "H:|-margin-[view(width)]", margin: 5)
+        self._alignSuperView("V:|-margin-[view]-margin-|", horizontal: "H:|-margin-[view(width)]", margin: 5)
     }
     
     func ezRightFill(margin: Int = 0){
@@ -256,6 +253,29 @@ extension UIView {
     }
     
     
+    func ezSetUnderMatch(view:UIView,margin: Int = 0){
+        self._sameSizeXAligned(view, vertical: "V:[view]-margin-[under(height)]", horizontal: "H:[under(==view)]", margin: margin,attribute:NSLayoutAttribute.CenterX)
+        
+    }
+    
+    func ezSetOverMatch(view:UIView,margin: Int = 0){
+        self._sameSizeXAligned(view, vertical: "V:[under(height)]-margin-[view]", horizontal: "H:[under(==view)]", margin: margin,attribute:NSLayoutAttribute.CenterX)
+        
+        
+    }
+    
+    func ezSetLeftOfMatch(view:UIView,margin: Int = 0){
+        self._sameSizeXAligned(view, vertical: "V:[under(==view)]", horizontal: "H:[view]-margin-[under(width)]", margin: margin,attribute:NSLayoutAttribute.CenterY)
+        
+        
+    }
+    
+    func ezSetRightOfMatch(view:UIView,margin: Int = 0){
+        self._sameSizeXAligned(view, vertical: "V:[under(==view)]", horizontal: "H:[under(width)]-margin-[view]", margin: margin,attribute:NSLayoutAttribute.CenterY)
+        
+        
+    }
+
 
 
     
--- AutoLayoutCookBook/ViewController.swift
+++ AutoLayoutCookBook/ViewController.swift
@@ -19,8 +19,77 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // One Tap
+        var tap = UITapGestureRecognizer(target: self, action: Selector("tapDelected:"))
+        tap.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(tap)
+        
+        // Double Tap
+        var dtap = UITapGestureRecognizer(target: self, action: Selector("doubleTapDelected:"))
+        dtap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(dtap)
+        tap.requireGestureRecognizerToFail(dtap)
+        
+        
     }
 
+    func doubleTapDelected(gesture:UITapGestureRecognizer){
+        var point = gesture.locationInView(self.view)
+        
+        self.move_button!.ezClearConstraints()
+        
+        var w3 = self.view.frame.width / 3.0
+        var h3 = self.view.frame.height/3.0
+        
+        if point.y < h3{
+            self.move_button!.ezTopFill(margin: 10)
+            
+        }else if (point.y > 2*h3){
+            self.move_button!.ezBottomFill(margin: 10)
+        }else if (point.x < w3){
+            self.move_button!.ezLeftFill(margin: 10)
+        }else if (point.x > 2*w3){
+            self.move_button!.ezRightFill(margin: 10)
+        }else{
+            self.move_button!.ezFill(margin: 10)
+        }
+        
+        self.move_button!.ezAnimateChage()
+        
+
+    }
+    func tapDelected(gesture:UITapGestureRecognizer){
+        var point = gesture.locationInView(self.view)
+        
+        var w3 = self.view.frame.width / 3.0
+        var h3 = self.view.frame.height/3.0
+        
+        if point.x < w3  && point.y < h3 {
+            self.move_button!.ezTopLeft(margin: 5)
+        }else if point.x < 2 * w3 && point.y < h3{
+            self.move_button!.ezTop(margin: 5)
+        }else if point.x > 2 * w3 && point.y < h3{
+            self.move_button!.ezTopRight(margin: 5)
+        } else if point.x < w3 &&   point.y > 2 * h3{
+            self.move_button!.ezBottomLeft(margin: 5)
+        }else if point.x < 2 * w3 &&   point.y > 2 * h3{
+            self.move_button!.ezBottom(margin: 5)
+        }else if point.x > 2 * w3 &&  point.y > 2 * h3{
+            self.move_button!.ezBottomRight(margin: 5)
+        }else if point.x < w3 && point.y >  h3{
+            self.move_button!.ezLeft(margin: 5)
+        }else if point.x > 2 * w3 && point.y >  h3{
+            self.move_button!.ezRight(margin: 5)
+        }else{
+            self.move_button!.ezCenter()
+        }
+        
+        
+        self.move_button!.ezAnimateChage()
+
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -50,75 +119,38 @@ class ViewController: UIViewController {
     
    
     @IBAction func centerAction(sender: AnyObject) {
-        self.move_button!.ezCenter()
+        self.move_button!.ezCenter(width:200,height:100)
         self.move_button!.ezAnimateChage()
     }
     
+    @IBAction func detatch(sender: AnyObject) {
+        self.one!.ezTopLeft(margin: 0)
+        self.two!.ezTopRight(margin: 0)
+        self.three!.ezBottomLeft(margin: 0)
+        self.four!.ezBottomRight(margin: 0)
+        self.view.ezAnimateChage()
+        
+    }
     
     @IBAction func addUnder(sender: AnyObject) {
-        
+    /*
         self.move_button!.ezCenter()
         self.two!.ezSetUnder(self.move_button!, margin: 0)
         self.one!.ezSetOver(self.move_button!, margin: 0)
         self.three!.ezSetRightOf(self.move_button!, margin: 0)
         self.four!.ezSetLeftOf(self.move_button!, margin: 0)
         self.two!.ezAnimateChage()
-    }
-    @IBAction func sendBotom(sender: AnyObject) {
+        */
         
-        self.move_button!.ezBottomFill(margin: 5)
-        self.move_button!.ezAnimateChage()
-    }
+        self.move_button!.ezCenter()
+        self.two!.ezSetUnderMatch(self.move_button!, margin: 0)
+        self.one!.ezSetOverMatch(self.move_button!, margin: 0)
+        self.three!.ezSetRightOfMatch(self.move_button!, margin: 0)
+        self.four!.ezSetLeftOfMatch(self.move_button!, margin: 0)
+        self.two!.ezAnimateChage()
+        
+        
 
-    @IBAction func sendTop(sender: AnyObject) {
-        self.move_button!.ezRightFill(margin: 5)
-        self.move_button!.ezAnimateChage()
     }
-   
-    @IBAction func addSon(sender: AnyObject) {
-        
-        var son = UIView()
-        
-        son.backgroundColor = UIColor.yellowColor()
-        
-        
-    }
-    
-    @IBAction func applyContrains(sender: AnyObject) {
-        
-        self.removeRelatedConstraints()
-        self.fillWithMargin(self.move_button!)
-      
-    }
-    
-    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
-        var point: CGPoint = touches.anyObject()!.locationInView(self.view)
-        
-        var w3 = self.view.frame.width / 3.0
-        var h3 = self.view.frame.height/3.0
-        
-        if point.x < w3  && point.y < h3 {
-            self.move_button!.ezTopLeft(margin: 5)
-        }else if point.x < 2 * w3 && point.y < h3{
-            self.move_button!.ezTop(margin: 5)
-        }else if point.x > 2 * w3 && point.y < h3{
-            self.move_button!.ezTopRight(margin: 5)
-        } else if point.x < w3 &&   point.y > 2 * h3{
-            self.move_button!.ezBottomLeft(margin: 5)
-        }else if point.x < 2 * w3 &&   point.y > 2 * h3{
-            self.move_button!.ezBottom(margin: 5)
-        }else if point.x > 2 * w3 &&  point.y > 2 * h3{
-            self.move_button!.ezBottomRight(margin: 5)
-        }else if point.x < w3 && point.y >  h3{
-            self.move_button!.ezLeft(margin: 5)
-        }else if point.x > 2 * w3 && point.y >  h3{
-            self.move_button!.ezRight(margin: 5)
-        }else{
-            self.move_button!.ezCenter()
-        }
-        
-        
-        self.move_button!.ezAnimateChage()
-    }
-}
+ }
 
